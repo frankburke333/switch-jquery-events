9373352d080136af71fb9300d1e3c9e097d72de0
frankburke333 <frank.burke@gmail.com>
2017-02-15 16:27:05 -0500

    add completed light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
+$(document).on('ready', function() {
 
+});
 
+$(function() {
+  $('div.switch button').on('click', function(){
+    if ($(this).attr('class') === 'on') {
+      $(this).toggleClass('on off');
+      $("body").toggleClass('light dark');
+      $('.status').html("it's too dark");
+    }
+    else {
+      $(this).toggleClass('off on');
+      $('.status').html("It's bright in here");
+      $("body").toggleClass('dark light');
+    }
+  });
+
+});
